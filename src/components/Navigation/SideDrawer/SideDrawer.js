7201902f384650c90466d12fd91439907eff18c5
Chang Lee <cleeucla@gmail.com>
2018-11-26 13:12:26 -0800

    sidedrawer and responsive css layout

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import classes from './SideDrawer.module.css';
 
+import Aux from '../../../hoc/Aux';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop'
+;
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="11%" />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop 
+        show={props.open} 
+        clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <Logo height="11%" />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
